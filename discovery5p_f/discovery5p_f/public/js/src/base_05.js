3e0cbcb8d04e621a9fc07ed2e1f175a5d72f71d8
haneulkimgreen <rlagksmf0918@gmail.com>
2018-02-11 13:12:50 +0900

    20180211

---
+++ discovery5p_f/discovery5p_f/public/js/src/base_05.js
@@ -0,0 +1,14 @@
+// base_05.js
+(function($){
+// jquery start ====================
+var wrap = $('#wrap');
+//불러올 파일 목록
+var baseUrl = './src/';
+var loadFile = {headBox: baseUrl +  'headBox.html',
+                content: baseUrl +  'main_content_05.html',
+                footBox: baseUrl +  'footBox.html'
+              };
+ImportFile('headBox', loadFile.headBox);
+ImportFile('contentBox', loadFile.content);
+ImportFile('footBox', loadFile.footBox);
+})(this.jQuery);
